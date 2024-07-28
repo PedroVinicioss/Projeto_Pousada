976338bfac3c74803a50cb958e58dc2b824e1685
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2024-07-28 17:09:19 -0300

    🔥Routes V1

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,15 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
 
-// Configurar middleware
 app.use(cors());
 app.use(express.json());
 
-// Iniciar o servidor
+const chaleRoutes = require('./routes/chale.routes');
+app.use('/api/chales', chaleRoutes);
+
+const hospedeRoutes = require('./routes/hospede.routes');
+app.use('/api/hospedes', hospedeRoutes);
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
