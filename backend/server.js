cb09977be2c3ef51be390698ff82ab93aa886fbd
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2024-07-30 10:43:27 -0300

    🔥Update Server.js

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,26 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
+const acompanhanteRoutes = require('./routes/acompanhante.routes');
+app.use('/acompanhante', acompanhanteRoutes);
+
 const chaleRoutes = require('./routes/chale.routes');
 app.use('/chales', chaleRoutes);
+
+const consumoRoutes = require('./routes/consumo.routes');
+app.use('/consumo', consumoRoutes);
+
+const fechamentoCaixaRoutes = require('./routes/fechamentoCaixa.routes');
+app.use('/fechamentoCaixa', fechamentoCaixaRoutes);
+
+const hospedagemRoutes = require('./routes/hospedagem.routes');
+app.use('/hospedagem', hospedagemRoutes);
+
+const hospedeRoutes = require('./routes/hospede.routes');
+app.use('/hospede', hospedeRoutes);
+
+const produtoRoutes = require('./routes/produto.routes');
+app.use('/produto', produtoRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
