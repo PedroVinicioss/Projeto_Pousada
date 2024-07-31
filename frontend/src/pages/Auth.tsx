48bb62bdcf6cca4379719f646fba63e78ee9d0c3
Pedro Vinicios <pedrooviniciossantos@gmail.com>
2024-07-31 19:06:30 -0300

    🚀 Auth V1

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -1,13 +1,22 @@
+import Picture from '../assets/capa.jpg';
+
 export function Auth(){
     return (
         <>
-            <form>
-                <label htmlFor="">E-mail</label>
-                <input type="e-mail" />
-                <br />
-                <label htmlFor="">Password</label>
-                <input type="password" />
-            </form>
+            <div className="main-Login">
+                <div className="main-picture">
+                    <img src={Picture} alt="" />
+                </div>
+                <div className="main-form">
+                <form>
+                    <label htmlFor="">E-mail</label>
+                    <input type="e-mail" />
+                    <br />
+                    <label htmlFor="">Password</label>
+                    <input type="password" />
+                </form>
+                </div>
+            </div>
         </>
     );
 }
